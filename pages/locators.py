e6420f5514b738ddad678f4676f921eab094e43c
Hvosten1 <dogrer@gmail.com>
2022-07-27 11:47:08 +0300

    Add login assertion methods to BasePage

--- pages/locators.py
+++ pages/locators.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "[id = login_form]")
     REGISTER_FORM = (By.CSS_SELECTOR, "[id = register_form]")
+
 
 class ProductPageLocators:
     PRODUCT_IMG = (By.CSS_SELECTOR, "[id = product_gallery")
@@ -15,3 +17,8 @@ class ProductPageLocators:
     PURCHASE_NAME = (By.CSS_SELECTOR, "div.alertinner strong")
     BUCKET_TOTAL = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info strong ")
     BOOK_PRICE = (By.CSS_SELECTOR, "div.col-sm-6.product_main p.price_color")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
