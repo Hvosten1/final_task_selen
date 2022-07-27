fbac9a936c7c753a89ac9b39cf1f5d40b37027c4
Hvosten1 <dogrer@gmail.com>
2022-07-27 11:24:12 +0300

    Add marks xfail

--- test_product_page.py
+++ test_product_page.py
@@ -23,7 +23,7 @@ def test_guest_can_add_product_to_basket(browser, link):
         time.sleep(5)
 
 @pytest.mark.parametrize('link', links)
-#@pytest.mark.xfail
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -37,6 +37,7 @@ def test_guest_cant_see_success_message(browser, link):
     page.should_not_be_success_message()
 
 @pytest.mark.parametrize('link', links)
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
