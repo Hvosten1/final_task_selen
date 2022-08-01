654315a57d74e204cb897487135f038a0250c027
Hvosten1 <dogrer@gmail.com>
2022-08-01 16:42:44 +0300

    Added review marks

--- test_product_page.py
+++ test_product_page.py
@@ -23,6 +23,7 @@ class TestUserAddToBasketFromProductPage:
         time.sleep(10)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         try:
             page = ProductPage(browser, link)
@@ -42,7 +43,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
-
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     try:
         page = ProductPage(browser, link)
@@ -86,14 +87,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
